e90550b3131d8ff90af0a58ef0c5d4642d19a97c
Agustin Eyzaguirre <aeyzaguirre@capp30121-0.cs.uchicago.edu>
2025-01-09 20:29:44 -0600

    working day 2

--- day1.py
+++ day1.py
@@ -33,28 +33,77 @@ computes_similarity_score(first_array, second_array)
 
 #------------------------------ Day 2 ---------------------------------#
 
-data = np.array([])
+data = []
 with open("data/day_2.csv") as file:
     reader = csv.reader(file)
     for row in reader:
-        data = np.append(data, row[0])
-        data = data.reshape(len(data), 1)
+        row = np.array(list(map(int, row[0].split(" "))))
+        data.append(row)
+
+data = np.array(data, dtype=object)
 
 MAX_DIFFERENCE = 3
+MIN_DIFFERENCE = 1
 safe = 0
-print("safe is", safe)
-for i, nums in enumerate(data):
-    nums = np.array(nums[0].split())
-    nums = nums.astype(int)
-    nums_ascending = np.sort(nums)
-    nums_descending = np.sort(nums)[::-1]
-    if np.array_equal(nums, nums_ascending) or np.array_equal(nums, nums_descending):
-        for i, n in enumerate(nums):
-            if i < (len(nums) - 1):
-                dif = abs(n - nums[i + 1])
-                if dif > MAX_DIFFERENCE:
-                    safe += 1
-                    break
+
+def is_increasing(arr):
+    return arr[0] < arr[-1]
+
+def all_increase(arr):
+    for i in range(0, len(arr) - 1):
+            if arr[i] > arr[i+1]:
+                return False
+    return True
+
+def all_decrease(arr):
+    for i in range(0, len(arr) - 1):
+            if arr[i] < arr[i+1]:
+                return False
+    return True
+
+def is_safe(arr):
+    if is_increasing(arr):
+        if all_increase(arr):
+            for i in range(0, len(arr) - 1):
+                first = arr[i]
+                second = arr[i+1]
+                difference = abs(second - first)
+                if difference > MAX_DIFFERENCE:
+                    return False
+    elif not is_increasing(arr):
+        if all_decrease(arr):
+            for i in range(0, len(arr) - 1):
+                first = arr[i]
+                second = arr[i+1]
+                difference = abs(second - first)
+                if difference > MAX_DIFFERENCE:
+                    return False
+    return True
+
+
+counter = 0
+l = []
+for d in data:
+    if is_increasing(d) and all_increase(d):
+        l.append(d)
+    elif not is_increasing(d) and all_decrease(d):
+        l.append(d)
+print(len(l))
+
+print(counter)
+
+
+     
+     
+         
+         
+               
+               
+          
+
+
+
+
                     
                     
 print(f"safe is: {safe}")
